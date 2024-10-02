026c26f4ee1bbfefb49a4bddb660d85f67e92cbe
GiselaFlores <gi.flores@bue.edu.ar>
2024-10-01 22:21:07 -0300

    intro js

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hola mundo js");
+
+//comentario de linea
+/* comentario debloque */ 
+
+//El ; no es necesariuo pero debería
+//La forma de escritura camelCase
+
+//Variable
+
+/* var globales *no se utilizan*
+   let locales/scope
+
+   const constantes
+*/
+
+var calculoEdad = 20;
+let nombre = "Pedrito";
+
+const IVA = 21;
+
+console.log(calculoEdad);
+console.log(nombre);
+console.log("El IVA Argentino es de: ", IVA);
